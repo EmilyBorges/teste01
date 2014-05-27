a36ab6301be4a6b3eab902560f595f03aaad17fe
emily_borges <emily_borges@10.1.48.138>
2014-05-27 14:36:04 -0300

    Inicialização de um projeto

---
+++ src/teste/Teste.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teste;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author emily_borges
+ */
+public class Teste {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null,"Testando");
+    }
+}
